9b15e36817de42708acf82e4629cf8301d3bdb16
GM-VIP <Github99VIP@gmail.com>
2025-02-20 02:12:12 -0500

    Update Unlock-Pass-VULTR.sh

--- gerador/Unlock-Pass-VULTR.sh
+++ gerador/Unlock-Pass-VULTR.sh
@@ -7,7 +7,7 @@ SCPinst="/etc/ger-inst" && [[ ! -d ${SCPinst} ]] && exit
 
 UNLOCK () {
 sudo apt-get install libpam-cracklib -y > /dev/null 2>&1
-wget https://raw.githubusercontent.com/ELITE-MUNDIAL/VIP/main/ArchUt/common-password -O /etc/pam.d/common-password > /dev/null 2>&1
+wget https://raw.githubusercontent.com/GM-VIP/SCRIPT/main/ArchUt/common-password -O /etc/pam.d/common-password > /dev/null 2>&1
     chmod +x /etc/pam.d/common-password
 msg -bar2
 echo -e "${cor[4]}Pass Alfanumerico Desactivado con EXITO"
