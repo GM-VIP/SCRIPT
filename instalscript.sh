b08d9e59d136db06804fb1c52dd11556df43e64f
GM-VIP <Github99VIP@gmail.com>
2025-07-04 13:21:02 -0600

    Update instalscript.sh

--- instalscript.sh
+++ instalscript.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#SYSTEM MODS BY: Razhiel && GHOST
+#04/07/2025
 
 cp .bashrc .bashrc.backup
 
@@ -27,13 +29,17 @@ sudo service ssh restart > /dev/null
 echo -e "\033[1;36m-----------------------------------------------------------------\033[0m"
 fi
 
+instinic () {
 killall apt apt-get
 dpkg --configure -a
 apt-get install software-properties-common -y
 apt-add-repository universe -y
 rm -rf /etc/localtime &>/dev/null
 ln -s /usr/share/zoneinfo/America/Lima /etc/localtime &>/dev/null
+apt-get install lolcat -y &>/dev/null
+apt-get install figlet &>/dev/null
 rm $(pwd)/$0 &> /dev/null
+}
 
 ### COLORES Y BARRA 
 msg () {
@@ -49,15 +55,67 @@ AZUL='\e[34m' && MAGENTA='\e[35m' && MAG='\033[1;36m' &&NEGRITO='\e[1m' && SEMCO
   "-bar2"|"-bar")cor="${VERMELHO}————————————————————————————————————————————————————" && echo -e "${SEMCOR}${cor}${SEMCOR}";;
  esac
 }
-clear
- msg -bar2
- msg -ama "        [ VPS - GHOST - SCRIPT \033[1;97m ❗ WELCOME❗\033[1;33m ]"
- echo -e  "\033[1;97m               EJECUTANDO ACTUALIZADOR  \033[1;34m "
- msg -bar2
+
+fun_bar () {
+comando[0]="$1"
+comando[1]="$2"
+ (
+[[ -e $HOME/fim ]] && rm $HOME/fim
+${comando[0]} -y > /dev/null 2>&1
+${comando[1]} -y > /dev/null 2>&1
+touch $HOME/fim
+ ) > /dev/null 2>&1 &
+echo -ne "         \033[1;33m["
+while true; do
+   for((i=0; i<40; i++)); do
+   echo -ne "\033[1;31m#"
+   sleep 0.1s
+   done
+   [[ -e $HOME/fim ]] && rm $HOME/fim && break
+   echo -e "\033[1;33m]"
+   sleep 1s
+   tput cuu1
+   tput dl1
+   echo -ne "         \033[1;33m["
+done
+echo -e "\033[1;33m]\033[1;31m -\033[1;32m 100%\033[1;37m"
+sleep 1
+}
+
+#Logo ADM_OS
+Logo () {
+figlet -f block " VIP_PERU SYSTEM" | lolcat
+echo -e "                  MODS SCRIPT BY: GHOST"
+}
+
+tput clear
+os_system
+repo "${vercin}"
+tput cup 6 0
+echo -e "              \e[7;3m   >>>> INICIANDO INSTALADOR<<<<   \e[0m"
+echo -e "          \e[1;97m    🔎 IDENTIFICANDO SISTEMA OPERATIVO   \e[0m"
+echo -e "          \e[1;32m           | $distro $vercin |"
+echo
+fun_bar 'instinic'
+clear && clear
+tput cup 6 0
+Logo
+sleep 3
+tput clear
+
+ tput cup 6 0
+ echo -e "\e[1;33m ▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎\e[0m"
+ tput cup 7 0
+ msg -verd "           [ V I P _ P E R U - O . S \033[1;97m By: ©⚜ GHOST ⚜\033[1;33m ]"
+ tput cup 8 0
+ echo -e "\e[1;33m ▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎\e[0m"
+ echo
+ tput cup 10 0
+ echo -e  "\033[1;33m      >>>> DESCARGANDO E INSTALANDO PAQUETES NECESARIOS <<<<<          \033[1;34m "
+ echo
  
 ## Script name
 SCRIPT_NAME=vpsmxup
-
 ## Install directory
 WORKING_DIR_ORIGINAL="$(pwd)"
 INSTALL_DIR_PARENT="/usr/local/vpsmxup/"
@@ -68,30 +126,38 @@ mkdir -p "/etc/vpsmxup/"
 
 ## Install/update
 if [ ! -d "$INSTALL_DIR" ]; then
-	echo -e  "\033[1;97m           Instalando Paquetes Prioritarios"
-	echo "           --------------------------------"
-	sleep 2
-	mkdir -p "$INSTALL_DIR_PARENT"
-	cd "$INSTALL_DIR_PARENT"
-    wget https://raw.githubusercontent.com/GM-VIP/SCRIPT/main/Install/zzupdate.default.conf -O /usr/local/vpsmxup/vpsmxup.default.conf  &> /dev/null
-	rm -rf /usr/local/vpsmxup/vpsmxup.sh
-    wget https://raw.githubusercontent.com/GM-VIP/SCRIPT/main/Install/zzupdate.sh -O /usr/local/vpsmxup/vpsmxup.sh &> /dev/null
-	chmod +x /usr/local/vpsmxup/vpsmxup.sh
-	rm -rf /usr/bin/vpsmxup
+    paqts () {
+    apt-get update -y
+    apt-get upgrade -y
+    apt-get install net-tools -y
+    apt-get install software-properties-common -y
+    apt-get install curl -y
+    sudo apt-add-repository universe -y
+    service ssh restart
+    }
+
+    echo
+    fun_bar 'paqts'
+    echo
+    
+   mkdir -p "$INSTALL_DIR_PARENT"
+   cd "$INSTALL_DIR_PARENT"
+   wget https://raw.githubusercontent.com/GM-VIP/SCRIPT/main/Install/zzupdate.default.conf -O /usr/local/vpsmxup/vpsmxup.default.conf  &> /dev/null
+   rm -rf /usr/local/vpsmxup/vpsmxup.sh
+   wget https://raw.githubusercontent.com/GM-VIP/SCRIPT/main/Install/zzupdate.sh -O /usr/local/vpsmxup/vpsmxup.sh &> /dev/null
+   chmod +x /usr/local/vpsmxup/vpsmxup.sh
+    rm -rf /usr/bin/vpsmxup
     wget https://raw.githubusercontent.com/GM-VIP/SCRIPT/main/Install/zzupdate.sh -O /usr/bin/vpsmxup &> /dev/null
-	chmod +x /usr/bin/vpsmxup
-	echo -e  "\033[1;97m              Copiando Instalador Interno "
-	
-	echo "           --------------------------------"	
-	msg -bar2
-	sleep 2
+    chmod +x /usr/bin/vpsmxup
+    
 else
-	echo ""
+echo ""
 fi
-sleep 2
+sleep 1
 
 ## Restore working directory
 cd $WORKING_DIR_ORIGINAL
 clear
 vpsmxup
+sleep 2
 #fin
