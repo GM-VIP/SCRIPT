9ce536fb0c0eb5c8df4a6c99130634afa757b061
GM-VIP <Github99VIP@gmail.com>
2025-09-09 16:28:12 -0500

    Update VPS-MX.sh

--- Install/VPS-MX.sh
+++ Install/VPS-MX.sh
@@ -24,6 +24,17 @@ rm -rf /etc/localtime &>/dev/null
 ln -s /usr/share/zoneinfo/America/Lima /etc/localtime &>/dev/null
 rm $(pwd)/$0 &> /dev/null
 
+# --- Asegurar UTF-8 globalmente (evita "�" en líneas Unicode) ---
+apt-get update -y >/dev/null 2>&1
+apt-get install -y locales >/dev/null 2>&1
+# Habilitar en_US.UTF-8 en /etc/locale.gen (si está comentado)
+sed -i 's/^# *en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
+locale-gen en_US.UTF-8 >/dev/null 2>&1
+update-locale LANG=en_US.UTF-8 LC_ALL=en_US.UTF-8 >/dev/null 2>&1
+# Exportar para la sesión actual del instalador
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+
 ### COLORES Y BARRA 
 msg () {
 BRAN='\033[1;37m' && VERMELHO='\e[31m' && VERDE='\e[32m' && AMARELO='\e[33m'
